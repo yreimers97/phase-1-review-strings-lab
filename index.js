5ab1a9f8e2ab3a9b589dca8da016b0a2f9d6849b
Yuri <+>
2024-01-28 22:02:40 -0500

    Completed Strings review

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Browny";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
